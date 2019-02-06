6ebcb62fc028a164d6cbdb19be9ada1451a8f68e
mixy <mixy0890@gmail.com>
2019-02-06 12:35:15 +0700

    06.02.2019

--- routes/mydisp.js
+++ routes/mydisp.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(req.session.logged == true) {
+  if(req.session.logged === true) {
     res.render('mydisp',{
       logget: req.session.logged
     });
